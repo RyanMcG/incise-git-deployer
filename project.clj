f0bd037296f389723edde87e06a4e256181c84c5
Ryan McGowan <ryan@ryanmcg.com>
2014-02-15 13:46:16 -0800

    Inc version to 0.1.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject incise-git-deployer "0.1.0-SNAPSHOT"
+(defproject incise-git-deployer "0.1.0"
   :description "The default git branch deployer for incise."
   :url "https://github.com/RyanMcG/incise-git-deployer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[incise-core "0.2.0-SNAPSHOT"]
+  :dependencies [[incise-core "0.2.0"]
                  [clj-jgit "0.4.0"]]
   :repl-options {:init-ns incise.repl}
   :main incise.core)
